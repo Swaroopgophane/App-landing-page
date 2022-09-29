0581f706cb09e4b9511fed553fad55d63fa1eb34
Swaroopgophane <swaroop.gophane2017@gmail.com>
2022-09-29 11:02:01 +0530

    media query changes

--- js/JsFile4.js
+++ js/JsFile4.js
@@ -3,17 +3,17 @@
 var HiddenBtn = document.getElementById('hidddenmenubtn');
 var HiddenSlidemenu = document.getElementById('hiddenmenulinks');
 
-HiddenSlidemenu.style.transform = 'translateX(650px)';
+HiddenSlidemenu.style.right = '-300px';
 
 HiddenBtn.onclick = function()
 {
-    if(HiddenSlidemenu.style.transform == 'translateX(650px)')
+    if(HiddenSlidemenu.style.right == '-300px')
     {
-        HiddenSlidemenu.style.transform = 'translateX(0px)'
+        HiddenSlidemenu.style.right = '0px'
     }
     else
     {
-        HiddenSlidemenu.style.transform = 'translateX(650px)'
+        HiddenSlidemenu.style.right = '-300px'
     }
 }
 
