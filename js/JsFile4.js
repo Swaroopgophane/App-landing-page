991e8bfac5ae370e8dac9758c566f453c77bf1cd
Swaroopgophane <swaroop.gophane2017@gmail.com>
2022-09-30 10:17:56 +0530

    my latest commit

--- js/JsFile4.js
+++ js/JsFile4.js
@@ -2,18 +2,23 @@
 
 var HiddenBtn = document.getElementById('hidddenmenubtn');
 var HiddenSlidemenu = document.getElementById('hiddenmenulinks');
+var CrossMenu = document.getElementById('Closemenu');
 
-HiddenSlidemenu.style.right = '-300px';
+HiddenSlidemenu.style.right = '-400px';
 
 HiddenBtn.onclick = function()
 {
-    if(HiddenSlidemenu.style.right == '-300px')
+    if(HiddenSlidemenu.style.right == '-400px')
     {
         HiddenSlidemenu.style.right = '0px'
     }
-    else
+}
+
+CrossMenu.onclick = function()
+{
+    if(HiddenSlidemenu.style.right == '0px')
     {
-        HiddenSlidemenu.style.right = '-300px'
+        HiddenSlidemenu.style.right = '-400px'
     }
 }
 
